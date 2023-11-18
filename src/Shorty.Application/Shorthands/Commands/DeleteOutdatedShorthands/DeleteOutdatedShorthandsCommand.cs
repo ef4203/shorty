4345f0459353198278d62211a9678b2ddc6e9896
Elias Frank <32647755+ef4203@users.noreply.github.com>
2023-11-19 00:38:29 +0100

    Updated packages

--- src/Shorty.Application/Shorthands/Commands/DeleteOutdatedShorthands/DeleteOutdatedShorthandsCommand.cs
+++ src/Shorty.Application/Shorthands/Commands/DeleteOutdatedShorthands/DeleteOutdatedShorthandsCommand.cs
@@ -2,4 +2,6 @@ namespace Shorty.Application.Shorthands.Commands.DeleteOutdatedShorthands;
 
 using MediatR;
 
+#pragma warning disable S2094
 public record DeleteOutdatedShorthandsCommand : IRequest;
+#pragma warning restore S2094
--- src/Shorty.Application/Shorthands/Queries/GetAllShorthands/GetAllShorthandsQuery.cs
+++ src/Shorty.Application/Shorthands/Queries/GetAllShorthands/GetAllShorthandsQuery.cs
@@ -2,4 +2,6 @@ namespace Shorty.Application.Shorthands.Queries.GetAllShorthands;
 
 using MediatR;
 
+#pragma warning disable S2094
 public record GetAllShorthandsQuery : IRequest<IEnumerable<ShorthandDto>>;
+#pragma warning restore S2094
